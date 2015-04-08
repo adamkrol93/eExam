7180b659069b806f843cd79fbd8deae1a00a3f88
Adam Król <adam.krol93@gmail.com>
2015-04-08 15:31:03 +0200

    SSBDI-125 Przygotowany interfejs TeacherEntityFacadeLocal.

--- src/main/java/pl/lodz/p/it/ssbd2015/moe/facades/TeacherEntityFacadeLocal.java
+++ src/main/java/pl/lodz/p/it/ssbd2015/moe/facades/TeacherEntityFacadeLocal.java
@@ -1,6 +1,7 @@
 package pl.lodz.p.it.ssbd2015.moe.facades;
 
 import pl.lodz.p.it.ssbd2015.entities.TeacherEntity;
+import pl.lodz.p.it.ssbd2015.entities.facade.Read;
 
 import javax.ejb.Local;
 import java.util.List;
@@ -8,9 +9,9 @@ import java.util.Optional;
 
 /**
  * @author Andrzej Kurczewski
+ * @author Adam Król
  */
 @Local
-public interface TeacherEntityFacadeLocal {
-    Optional<TeacherEntity> find(Object id);
-    List<TeacherEntity> findAll();
+public interface TeacherEntityFacadeLocal extends Read<Long,TeacherEntity>{
+    Optional<TeacherEntity> findByLogin(Object login);
 }
