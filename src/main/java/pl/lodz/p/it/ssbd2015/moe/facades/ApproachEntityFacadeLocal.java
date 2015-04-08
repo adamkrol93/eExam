139fb1ed9a4e4ad886a0de106405aca83020b75c
Adam Król <adam.krol93@gmail.com>
2015-04-08 15:29:25 +0200

    SSBDI-125 Stworzony interfejs ApporachEntityFacadeLocal

--- src/main/java/pl/lodz/p/it/ssbd2015/moe/facades/ApproachEntityFacadeLocal.java
+++ src/main/java/pl/lodz/p/it/ssbd2015/moe/facades/ApproachEntityFacadeLocal.java
@@ -1,6 +1,8 @@
 package pl.lodz.p.it.ssbd2015.moe.facades;
 
 import pl.lodz.p.it.ssbd2015.entities.ApproachEntity;
+import pl.lodz.p.it.ssbd2015.entities.facade.Merge;
+import pl.lodz.p.it.ssbd2015.entities.facade.Read;
 
 import javax.ejb.Local;
 import java.util.List;
@@ -10,8 +12,5 @@ import java.util.Optional;
  * @author Andrzej Kurczewski
  */
 @Local
-public interface ApproachEntityFacadeLocal {
-    void update(ApproachEntity approachEntity);
-    Optional<ApproachEntity> find(Object id);
-    List<ApproachEntity> findAll();
+public interface ApproachEntityFacadeLocal extends Merge<Long,ApproachEntity>, Read<Long,ApproachEntity>{
 }
