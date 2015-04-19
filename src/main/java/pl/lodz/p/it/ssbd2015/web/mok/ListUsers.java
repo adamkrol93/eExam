effc227a68094b462989929f4de61004f0ce8287
Michał Sośnicki <180692@edu.p.lodz.pl>
2015-04-19 15:11:33 +0200

    SSBDI-185: Nie warto używać commandButtonów jako zwykłych linków, przenosi to bez potrzeby informacje o powiązaniach między stronami z xhtmli do beanów. Jeżeli inicjalizacja modelu nie wymaga parametru z widoku (np. loginu), to lepiej to zrobić w metodzie @PostConstruct. @ViewScoped muszą być serializowalne, ale w tym wypadku wystarczy @RequestScoped (jak sam zauważyłeś). DataModel jest tu niepotrzebny jak przenieśliśmy linki do .xhtmli.

--- src/main/java/pl/lodz/p/it/ssbd2015/web/mok/ListUsers.java
+++ src/main/java/pl/lodz/p/it/ssbd2015/web/mok/ListUsers.java
@@ -3,46 +3,33 @@ package pl.lodz.p.it.ssbd2015.web.mok;
 import pl.lodz.p.it.ssbd2015.entities.PersonEntity;
 import pl.lodz.p.it.ssbd2015.mok.services.PeopleServiceRemote;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
-import javax.faces.model.DataModel;
-import javax.faces.model.ListDataModel;
 import java.util.List;
 
 /**
- * Created by adam on 19.04.15.
+ * Backing bean do strony z listą wszystkich użytkowników.
+ * @author Created by adam on 19.04.15.
+ * @author Michał Sośnicki
  */
 @ManagedBean(name = "listUsersMOK")
-@ViewScoped
+@RequestScoped
 public class ListUsers {
 
     @EJB
     private PeopleServiceRemote peopleService;
 
-    private DataModel<PersonEntity> personEntities;
+    private List<PersonEntity> personEntities;
 
-
-    public List<PersonEntity> findAllPersons()
-    {
-        return peopleService.findAllPeron();
+    @PostConstruct
+    private void initializeModel() {
+        personEntities = peopleService.findAllPeron();
     }
 
-    public DataModel<PersonEntity> getPersonEntities() {
-        if(personEntities == null)
-        {
-            personEntities = new ListDataModel<>(findAllPersons());
-        }
+    public List<PersonEntity> getPersonEntities() {
         return personEntities;
     }
 
-    public String editPerson()
-    {
-        return "editUser?faces-redirect=true&login=" + personEntities.getRowData().getLogin();
-    }
-
-    public String showPerson()
-    {
-        return "showUser?faces-redirect=true&login=" + personEntities.getRowData().getLogin();
-    }
 }
