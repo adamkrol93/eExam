589946034cb3428618ef66709b0d06ad55a22728
marcin1238 <marcinkabza1238@gmail.com>
2015-04-22 19:37:13 +0200

    Przygotowałem widok oraz ManagedBean. Wszystko fajnie działa i można wyszukiwać już.
    Issue: SSBDI-198

---
+++ src/main/java/pl/lodz/p/it/ssbd2015/web/mok/SearchUsers.java
@@ -0,0 +1,60 @@
+package pl.lodz.p.it.ssbd2015.web.mok;
+import pl.lodz.p.it.ssbd2015.entities.PersonEntity;
+import pl.lodz.p.it.ssbd2015.mok.services.PeopleServiceRemote;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.util.List;
+
+/**
+ * Backing bean do strony z listą wszystkich użytkowników.
+ * @author Created by Marcin on 2015-04-22.
+ */
+@ManagedBean(name = "searchUsersMOK")
+@RequestScoped
+public class SearchUsers {
+
+    @EJB
+    private PeopleServiceRemote peopleService;
+
+    private List<PersonEntity> personEntities;
+
+    private String phrase;
+    private String results;
+
+    public void search() {
+        if(phrase != null)
+        {
+            this.personEntities = peopleService.findPeopleByPhrase(phrase);
+        }
+        else
+        {
+            this.results = "#{i18n['mok.search.nothing']}";
+        }
+    }
+
+    public List<PersonEntity> getPersonEntities() {
+        return personEntities;
+    }
+    public void setPersonEntities(List<PersonEntity> personEntities)
+    {
+        this.personEntities = personEntities;
+    }
+    public void setPhrase(String phrase)
+    {
+        this.phrase = phrase;
+    }
+    public String getPhrase()
+    {
+        return this.phrase;
+    }
+    public String getResults()
+    {
+        return this.results;
+    }
+    public void setResults(String results)
+    {
+        this.results = results;
+    }
+}
