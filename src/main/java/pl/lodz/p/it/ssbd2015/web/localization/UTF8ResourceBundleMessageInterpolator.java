c7dbd1ee31b0dc76bd6534da9c0781a5e7e33343
Andrzej Kurczewski <akurc24k@gmail.com>
2015-04-22 16:56:30 +0200

    SSBDI-201 Dokumentacja

--- src/main/java/pl/lodz/p/it/ssbd2015/web/localization/UTF8ResourceBundleMessageInterpolator.java
+++ src/main/java/pl/lodz/p/it/ssbd2015/web/localization/UTF8ResourceBundleMessageInterpolator.java
@@ -3,6 +3,7 @@ package pl.lodz.p.it.ssbd2015.web.localization;
 import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator;
 
 /**
+ * Własny interpolator, pozwalający wykorzystywać pliki properties kodowane w UTF-8
  * @author Andrzej Kurczewski
  */
 public class UTF8ResourceBundleMessageInterpolator extends ResourceBundleMessageInterpolator {
