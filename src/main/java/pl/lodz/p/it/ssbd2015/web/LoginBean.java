da51afda91819a4635642aa57bfc57a06d26566a
pjurewicz <pjurewicz93@gmail.com>
2015-04-29 23:03:08 +0200

    SSBDI-226 menu bazujące na sprawdzaniu poziomów dostępu i poprawki widoków

--- src/main/java/pl/lodz/p/it/ssbd2015/web/LoginBean.java
+++ src/main/java/pl/lodz/p/it/ssbd2015/web/LoginBean.java
@@ -2,35 +2,33 @@ package pl.lodz.p.it.ssbd2015.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.lodz.p.it.ssbd2015.entities.Groups;
 import pl.lodz.p.it.ssbd2015.entities.PersonEntity;
 import pl.lodz.p.it.ssbd2015.mok.exceptions.PersonEntityNotFoundException;
 import pl.lodz.p.it.ssbd2015.mok.services.PeopleServiceRemote;
 import pl.lodz.p.it.ssbd2015.mok.services.PersonServiceRemote;
 
-import javax.annotation.Resource;
 import javax.ejb.EJB;
-import javax.ejb.SessionContext;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 
 /**
  * Bean zapewnia obsługę poprawnego zalogowania się użytkownika
+ *
  * @author Tobiasz Kowalski
  */
 @ManagedBean(name = "loginBean")
 @SessionScoped
-public class LoginBean implements Serializable{
+public class LoginBean implements Serializable {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    private PersonEntity loggernUser;
+    private PersonEntity loggedUser;
 
 
     @EJB
@@ -41,6 +39,7 @@ public class LoginBean implements Serializable{
 
     /**
      * Metoda zapisujaca czas, ip oraz login do bazy zaraz po zalogowaniu się użytkownika
+     *
      * @return metoda zwraca wartość true gdy użytkownik zalogował się poprawnie do systemu
      * @throws PersonEntityNotFoundException
      */
@@ -49,26 +48,42 @@ public class LoginBean implements Serializable{
         FacesContext context = FacesContext.getCurrentInstance();
         HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
 
-
         Calendar time = Calendar.getInstance();
 
-
         logger.info("Uruchomienie funkcji logowania");
+        if (FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal() != null && loggedUser == null) {
 
-        if(FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal() != null && loggernUser == null){
-
-            loggernUser = personService.getPerson(FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal().getName());
+            loggedUser = personService.getPerson(FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal().getName());
             logger.info("Ktoś się właśnie zalogował");
-            peopleService.correctLogin(loggernUser.getLogin(), request.getRemoteAddr(), time);
+            peopleService.correctLogin(loggedUser.getLogin(), request.getRemoteAddr(), time);
             return true;
         }
-
-        if(FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal() == null && loggernUser != null){
-            loggernUser =null;
+        if (FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal() == null && loggedUser != null) {
+            loggedUser = null;
             return false;
         }
 
         return false;
+    }
+
+    public boolean isAdministrator() throws PersonEntityNotFoundException {
+        return peopleService.isAdministrator();
+    }
+
+    public boolean isStudent() throws PersonEntityNotFoundException {
+        return peopleService.isStudent();
+    }
+
+    public boolean isTeacher() throws PersonEntityNotFoundException {
+        return peopleService.isTeacher();
+    }
+
+    public boolean isGuardian() throws PersonEntityNotFoundException {
+        return peopleService.isGuardian();
+    }
+
+    public boolean isExaminer() throws PersonEntityNotFoundException {
+        return peopleService.isExaminer();
     }
 
 }
