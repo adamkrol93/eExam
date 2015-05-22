41b8338eb13a096ee9d5efa3596b999cf3e48074
Adam Król <adam.krol93@gmail.com>
2015-05-22 19:06:19 +0200

    SSBDI-345: Wprowadzona nawigacja między szczegółami i raportem dla egzaminów.

--- src/main/java/pl/lodz/p/it/ssbd2015/web/mze/ListExams.java
+++ src/main/java/pl/lodz/p/it/ssbd2015/web/mze/ListExams.java
@@ -4,9 +4,12 @@ import pl.lodz.p.it.ssbd2015.entities.ExamEntity;
 import pl.lodz.p.it.ssbd2015.mze.services.ExamListServiceRemote;
 import pl.lodz.p.it.ssbd2015.web.context.BaseContextBean;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 import java.util.List;
 
 /**
@@ -17,20 +20,60 @@ import java.util.List;
 @ViewScoped
 public class ListExams extends BaseContextBean {
 
+    private static final long serialVersionUID = 1L;
+
     @EJB
     private ExamListServiceRemote examListServiceRemote;
 
     private List<ExamEntity> examEntities;
 
+    private transient DataModel<ExamEntity> exams;
+
+
+    /**
+     * Metoda korzysta z dobrodziejstw contextMapy i ustawia
+     * odpowiednie identyfikator do przejścia do szczegółów egzaminu
+     * @return strona na którą przekierowywuje po skończonej operacji
+     */
+    public String gotoDetails()
+    {
+        long examId = exams.getRowData().getId();
+        setContext(ShowExamDetails.class, bean -> bean.setId(examId));
+        return String.format("showExam?uuid=%s&faces-redirect=true", getUuid());
+    }
+
+    /**
+     * Metoda korzysta z dobrodziejstw contextMapy i ustawia
+     * odpowiednie identyfikator do przejścia do raportu dotyczącego egzaminu
+     * @return strona na którą przekierowywuje po skończonej operacji
+     */
+    public String gotoReport()
+    {
+        long examId = exams.getRowData().getId();
+        setContext(ShowExamDetails.class, bean -> bean.setId(examId));
+        return String.format("showExamReport?uuid=%s&faces-redirect=true", getUuid());
+    }
+
     /**
      * Metoda pobiera z endpointu ({@link ExamListServiceRemote} wszystkie egzaminy w systemie.
-     * @return Lista wszystkich egzaminów
      */
+    @PostConstruct
+    private void initializeModel()
+    {
+        this.examEntities = examListServiceRemote.findAll();
+    }
+
+
     public List<ExamEntity> getExamEntities() {
-        if (examEntities == null) {
-            examEntities = examListServiceRemote.findAll();
-        }
         return examEntities;
     }
 
+
+    public DataModel<ExamEntity> getExams() {
+        if(exams == null)
+        {
+            this.exams = new ListDataModel<>(examEntities);
+        }
+        return exams;
+    }
 }
