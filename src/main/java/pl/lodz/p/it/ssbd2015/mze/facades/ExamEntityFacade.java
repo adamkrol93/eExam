e973681addcf29b8f5c1d9e7da552880b4eaf688
Andrzej Kurczewski <akurc24k@gmail.com>
2015-05-24 17:47:20 +0200

    SSBDI-349 Wyłapanie wyjątków dla ExamEntityFacade.edit

--- src/main/java/pl/lodz/p/it/ssbd2015/mze/facades/ExamEntityFacade.java
+++ src/main/java/pl/lodz/p/it/ssbd2015/mze/facades/ExamEntityFacade.java
@@ -11,10 +11,7 @@ import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.interceptor.Interceptors;
-import javax.persistence.EntityExistsException;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceException;
+import javax.persistence.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +19,7 @@ import java.util.Optional;
  * Implementacja interfejsu {@link ExamEntityFacadeLocal}, pozwala na zarządzanie Egzaminami.
  * @author Michał Sośnicki <sosnicki.michal@gmail.com>
  * @author Piotr Jurewicz
+ * @author Andrzej Kurczewski
  */
 @Stateless(name = "pl.lodz.p.it.ssbd2015.mze.facades.ExamEntityFacade")
 @TransactionAttribute(TransactionAttributeType.MANDATORY)
@@ -72,7 +70,27 @@ public class ExamEntityFacade implements ExamEntityFacadeLocal {
     @Override
     @RolesAllowed({"EDIT_EXAM_MZE", "ADD_TEACHER_TO_EXAM_MZE", "REMOVE_QUESTION_FROM_EXAM_MZE", "REMOVE_TEACHER_FROM_EXAM_MZE"})
     public void edit(ExamEntity entity) throws ApplicationBaseException {
-        ExamEntityFacadeLocal.super.edit(entity);
+        try {
+            ExamEntityFacadeLocal.super.edit(entity);
+        } catch (IllegalArgumentException ex) {
+            throw new ExamIllegalArgumentException(entity + " is an illegal argument to Merge.edit(e)", ex);
+        } catch (OptimisticLockException ex) {
+            throw new ExamOptimisticLockException(entity + " is being edit by someone else", ex);
+        } catch (PersistenceException ex) {
+            if (ex.getMessage().contains("exam_title_key")) {
+                throw new ExamTitleNotUnique("Title " + entity.getTitle() + " is not unique.", ex);
+            } else if (ex.getMessage().contains("exam_exam_creator_id_fkey")) {
+                throw new ExamCreatorForeignKeyException("Creator id is incorrect in entity: " + entity, ex);
+            } else if (ex.getMessage().contains("exam_exam_modifier_id_fkey")) {
+                throw new ExamModifierForeignKeyException("Modifier id is incorrect in entity: " + entity, ex);
+            } else if (ex.getMessage().contains("exam_groups")) {
+                throw new ExamTeacherForeignKeyException("Failed to join Exam and Teacher objects in ManyToMany relationship", ex);
+            } else if (ex.getMessage().contains("exam_question")) {
+                throw new ExamQuestionForeignKeyException("Failed to join Exam and Question objects in ManyToMany relationship", ex);
+            } else {
+                throw new ExamManagementException("Persisting " + entity + " violated a database constraint.", ex);
+            }
+        }
     }
 
     @Override
