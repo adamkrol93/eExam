e61279ea3ee3b1aa5a64750347faa5766161da54
Tobiasz Kowalski <180595@edu.p.lodz.pl>
2015-05-27 13:16:45 +0200

    SSBDI-413 Dodanie przejście do edycji Pytania z wykorzystaniem ContextMapy

--- src/main/java/pl/lodz/p/it/ssbd2015/web/mze/ListQuestions.java
+++ src/main/java/pl/lodz/p/it/ssbd2015/web/mze/ListQuestions.java
@@ -1,12 +1,16 @@
 package pl.lodz.p.it.ssbd2015.web.mze;
 
+
 import pl.lodz.p.it.ssbd2015.entities.QuestionEntity;
 import pl.lodz.p.it.ssbd2015.mze.services.ExamsServiceRemote;
+import pl.lodz.p.it.ssbd2015.web.context.BaseContextBean;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 import java.util.List;
 
 /**
@@ -14,13 +18,23 @@ import java.util.List;
  * @author Created by Tobiasz Kowalski on 17.05.15.
  */
 @ManagedBean(name = "listQuestionsMZE")
-@RequestScoped
-public class ListQuestions {
+@ViewScoped
+public class ListQuestions extends BaseContextBean{
+
+    private static final long serialVersionUID = 1L;
 
     @EJB
     private ExamsServiceRemote examsService;
 
     private List<QuestionEntity> questionEntities;
+
+    private transient DataModel<QuestionEntity> questions;
+
+    public String gotoEdit() {
+        long examId = questions.getRowData().getId();
+        setContext(EditExam.class, bean -> bean.setId(examId));
+        return String.format("editQuestion?uuid=%s&faces-redirect=true", getUuid());
+    }
 
     @PostConstruct
     private void initializeModel(){
@@ -29,6 +43,14 @@ public class ListQuestions {
 
     public List<QuestionEntity> getQuestionEntities(){
         return questionEntities;
+    }
+
+    public DataModel<QuestionEntity> getQuestions() {
+        if(questions == null)
+        {
+            this.questions = new ListDataModel<>(questionEntities);
+        }
+        return questions;
     }
 
     public static String cut(String message){
